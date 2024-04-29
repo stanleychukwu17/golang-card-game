3e8b81ff44b750d756b283bd2d2cf8cdbcf581ff
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-04-29 12:58:45 +0100

    make sure that every function in the deck.go file is used either in the testing file or in the main.go file

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import "fmt"
 
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
-	// hands, remainingCards := deal(cards, 5)
+
+	// shuffle the card
+	cards.shuffle()
+
+	// deal the cards to the first and second player
+	firstPlayer, remainingCards := deal(cards, 5)
+	secondPlayer, _ := deal(remainingCards, 5)
+
+	// print the cards
+	cards.print()
+
+	fmt.Printf("\nFirst player: %v \nSecond player: %v", firstPlayer, secondPlayer)
 }
